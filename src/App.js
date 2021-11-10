86e0420f8023984557aff57767fcb494030b2d57
padmini v <padmini.v@perfios.com>
2021-11-10 16:12:51 +0530

    ADDED: ability to display hello messgae with user input name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      title: 'Hello from Bridgelabz'
+      userName: ''
     }
   }
 
@@ -17,13 +17,23 @@ class App extends React.Component {
     console.log("save button is clicked!",$event);
     window.open(this.url, "_blank");
   }
+
+  onNameChange = (event) => {
+    console.log("value is ",event.target.value);
+    this.setState({userName:event.target.value})
+  }
   render() {
     return (
+      <>
       <div>
-        <h1>{this.state.title}</h1>
+        <h1>Hello {this.state.userName} from Bridgelabz</h1>
         <img src={logo} onClick={this.onClick}
           alt="The Bridgelabz logo : a Bridge to employment through lab works"/>
       </div>
+      <div>
+        <input onChange={this.onNameChange} />
+      </div>
+      </>
     );   
   }
 }
