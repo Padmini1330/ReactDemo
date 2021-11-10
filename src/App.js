68419c5d23a6b917fdae5a37570c26ed36f4d6ee
padmini v <padmini.v@perfios.com>
2021-11-10 16:34:20 +0530

    ADDED: validation for username and error to display when validation fails

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      userName: ''
+      userName: '',
+      nameError: ''
     }
   }
 
@@ -20,7 +21,14 @@ class App extends React.Component {
 
   onNameChange = (event) => {
     console.log("value is ",event.target.value);
-    this.setState({userName:event.target.value})
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    this.setState({userName:event.target.value});
+    if(nameRegex.test(event.target.value)){
+      this.setState({nameError: ""})
+    }
+    else{
+      this.setState({nameError: "Name is Incorrect"})
+    }
   }
   render() {
     return (
@@ -31,7 +39,8 @@ class App extends React.Component {
           alt="The Bridgelabz logo : a Bridge to employment through lab works"/>
       </div>
       <div>
-        <input onChange={this.onNameChange} />
+        <input class="textField" onChange={this.onNameChange} />
+        <span className= "error-output">{this.state.nameError}</span>
       </div>
       </>
     );   
