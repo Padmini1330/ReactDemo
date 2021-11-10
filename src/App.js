8e6074bcd4d328088a9f78edb21f81f1a2e68331
padmini v <padmini.v@perfios.com>
2021-11-10 15:56:04 +0530

    ADDED: Ability to display logo using property binding technique

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
+import logo from '../src/assets/images/BL_logo.jpg';
 
 class App extends React.Component {
 
   constructor() {
     super()
     this.state = {
-      title: 'Hello from Bridgelabz',
-      subTitle: 'welcome'
+      title: 'Hello from Bridgelabz'
     }
   }
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <h1>{this.state.subTitle}</h1>
+        <img src={logo}
+          alt="The Bridgelabz logo : a Bridge to employment through lab works"/>
       </div>
     );   
   }
